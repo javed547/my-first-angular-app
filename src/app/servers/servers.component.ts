8c351406d07470096d2995670b91b0517b2f0405
Mohd Javed Khan <jal90javed@gmail.com>
2020-04-10 12:17:03 +0530

    last commit from hands on angular app

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Initial Value';
+  userName = '';
+  resetUserName = true;
+  serverCreated = false;
+  servers = ['Test Server 1', 'Test Server 2'];
+  displayParagraph = 'block';
+  paragraphLog = [];
+  paragraphCounter = 0;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name of server is : ' + this.serverName;
+  }
+
+  onUpdateServerName(event){
+    this.serverName = event.target.value;
+  }
+
+  onUpdateUserName(event){
+    if (event.target.value.length > 0){
+      this.resetUserName = false;
+    }
+  }
+
+  clearUserName(){
+    this.userName = '';
+    this.resetUserName = true;
+  }
+
+  toggleParagraphDisplay(){
+    this.paragraphLog.push(new Date());
+    if (this.displayParagraph === 'block'){
+      this.displayParagraph = 'none';
+    } else {
+      this.displayParagraph = 'block';
+    }
+  }
+
+  getDisplayProperty(){
+    return this.displayParagraph;
+  }
+
+  getColor(paragraph){
+    if (paragraph > 3){
+      return 'blue';
+    } else {
+      return 'transparent';
+    }
+  }
 }
